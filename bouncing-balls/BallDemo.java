a27ae48fef13c3577ac46b6f1d4c5253a7a6be3d
B Kleinen <kleinen@infrastructure.de>
2018-06-05 12:41:00 +0200

    added drawDemo

--- bouncing-balls/BallDemo.java
+++ bouncing-balls/BallDemo.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Rectangle;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -55,4 +57,50 @@ public class BallDemo
             }
         }
     }
+     /**
+     * Demonstrate some of the drawing operations that are
+     * available on a Canvas object.
+     */
+    public void drawDemo()
+    {
+        myCanvas.setFont(new Font("helvetica", Font.BOLD, 14));
+        myCanvas.setForegroundColor(Color.red);
+
+        myCanvas.drawString("We can draw text, ...", 20, 30);
+        myCanvas.wait(1000);
+
+        myCanvas.setForegroundColor(Color.black);
+        myCanvas.drawString("...draw lines...", 60, 60);
+        myCanvas.wait(500);
+        myCanvas.setForegroundColor(Color.gray);
+        myCanvas.drawLine(200, 20, 300, 50);
+        myCanvas.wait(500);
+        myCanvas.setForegroundColor(Color.blue);
+        myCanvas.drawLine(220, 100, 370, 40);
+        myCanvas.wait(500);
+        myCanvas.setForegroundColor(Color.green);
+        myCanvas.drawLine(290, 10, 320, 120);
+        myCanvas.wait(1000);
+
+        myCanvas.setForegroundColor(Color.gray);
+        myCanvas.drawString("...and shapes!", 110, 90);
+
+        myCanvas.setForegroundColor(Color.red);
+
+        // the shape to draw and move
+        int xPos = 10;
+        Rectangle rect = new Rectangle(xPos, 150, 30, 20);
+
+        // move the rectangle across the screen
+        for(int i = 0; i < 200; i ++) {
+            myCanvas.fill(rect);
+            myCanvas.wait(10);
+            myCanvas.erase(rect);
+            xPos++;
+            rect.setLocation(xPos, 150);
+        }
+        // at the end of the move, draw once more so that it remains visible
+        myCanvas.fill(rect);
+    }
+
 }
